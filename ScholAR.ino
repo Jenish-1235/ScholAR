c8f0cff3c1b27b8767592b173a9ef773ce534ae1
Jenish-1235 <jenishtogadiya549@gmail.com>
2025-08-05 18:36:00 +0530

    prog : camera initialised

--- ScholAR.ino
+++ ScholAR.ino
@@ -1,7 +1,10 @@
 #include <WiFi.h>
+#include <esp_camera.h>
 
-const char* ssid = "testing";
-const char* password = "jenish1235";
+
+// Wifi connection variables
+const char* ssid = "Create Impact";
+const char* password = "RICE123!@#";
 
 const int LED_PIN = LED_BUILTIN;
 
@@ -10,6 +13,29 @@ unsigned long blinkInterval = 1000;
 bool ledState = false;
 
 bool wifiConnected = false;
+
+// Camera pin mapping for Seeed XIAO ESP32-S3
+#define PWDN_GPIO_NUM     -1
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM     10
+#define SIOD_GPIO_NUM     40
+#define SIOC_GPIO_NUM     39
+
+#define Y9_GPIO_NUM       48
+#define Y8_GPIO_NUM       11
+#define Y7_GPIO_NUM       12
+#define Y6_GPIO_NUM       14
+#define Y5_GPIO_NUM       16
+#define Y4_GPIO_NUM       18
+#define Y3_GPIO_NUM       17
+#define Y2_GPIO_NUM       15
+
+#define VSYNC_GPIO_NUM    38
+#define HREF_GPIO_NUM     47
+#define PCLK_GPIO_NUM     13
+
+
+
 
 void setup(){
   Serial.begin(115200);
@@ -24,6 +50,10 @@ void setup(){
   digitalWrite(LED_PIN, LOW);
 
   connectToWifi();
+  if(!setupCamera()){
+    Serial.println("Camera Failed.");
+    while(true) delay(1000);
+  }
 }
 
 void loop(){
@@ -37,7 +67,7 @@ void loop(){
 }
 
 void connectToWifi(){
-  Serial.printf("Connecting to SSI: %s\n", ssid);
+  Serial.printf("Connecting to SSID: %s\n", ssid);
   WiFi.begin(ssid, password);
 
   int attempts = 0;
@@ -58,5 +88,46 @@ void connectToWifi(){
     blinkInterval = 300;
     Serial.println("\n Wifi connection failed.");
   }
+
+}
+
+bool setupCamera(){
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1       = Y3_GPIO_NUM;
+  config.pin_d2       = Y4_GPIO_NUM;
+  config.pin_d3       = Y5_GPIO_NUM;
+  config.pin_d4       = Y6_GPIO_NUM;
+  config.pin_d5       = Y7_GPIO_NUM;
+  config.pin_d6       = Y8_GPIO_NUM;
+  config.pin_d7       = Y9_GPIO_NUM;
+  config.pin_xclk     = XCLK_GPIO_NUM;
+  config.pin_pclk     = PCLK_GPIO_NUM;
+  config.pin_vsync    = VSYNC_GPIO_NUM;
+  config.pin_href     = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn     = PWDN_GPIO_NUM;
+  config.pin_reset    = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  config.frame_size = FRAMESIZE_QVGA;
+  config.jpeg_quality = 15;
+  config.fb_count = 1;
+
+
+  esp_err_t err = esp_camera_init(&config);
+  if(err != ESP_OK){
+    Serial.println("Failed to initialise camera");
+    return false;
+  }
+
+  Serial.println("Initialised Camera");
+  return true;
+
 
 }
